3fc94c283f0d8281f45250fb4e7ca72b9b894cb2
CamillaDahlstroem <CamillaDahlstroem@users.noreply.github.com>
2019-02-07 11:13:46 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,5 +18,12 @@ export const text = {
         'Fobi for skade og sykdom, for eksempel redsel for sprøytestikk, blod og tannlegeskrekk.',
         'Fobi for bestemte situasjoner, for eksempel klaustrofobi, høydeskrekk og flyskrekk.',
     ]
+    CAUSES_1: 'Årsaken til fobier er oftest sammensatt. Det kan begynne med en panikklidelse der personen utvikler frykt for den situasjonen hvor hen fikk det første anfallet av panikk.',
+    CAUSES_2: [
+        'Tvillingsundersøkelser har vist at det foreligger en arvelig sårbarhet for å utvikle fobier. ',
+        'Denne sårbarheten samvirker med ytre hendelser og kan gi fobi uten at det har forekommet et panikkanfall. ',
+    ]
+    SYMPTOMS_1: 'Symptomene er angst, uro, hjertebank, skjelving, ubehag i brystet, pustevanskeligheter, kvelningsfornemmelse, svimmelhet og følelse av uvirkelighet når personen utsettes for det fobiske stimulus.',
+    SYMPTOMS_2: 'Frykt for å dø er et vanlig symptom.',
 }
 
