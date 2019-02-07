18f770e47c3956d9b21e35520bcde7c720449610
CamillaDahlstroem <CamillaDahlstroem@users.noreply.github.com>
2019-02-07 11:18:31 +0100

    Update index.js
    retter opp noen kommafeil :p

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,19 +10,19 @@ export const text = {
     CATEGORIES_1: [
         'Spesifikke fobier er svært utbredt i befolkningen og er den vanligste formen for angst. Mens man ved agorafobi og sosial fobi frykter et helt spekter av situasjoner, er frykten ved spesifikk fobi avgrenset til én situasjon, eller ett objekt, slik navnet sier.',
         'Fobier inndeles i tre grupper: 1) spesifikk eller enkel fobi, 2) sosial fobi, angst for å snakke i forsamlinger og angst for å spise eller drikke sammen med andre og, 3) agorafobi, angst for torg og samlingssteder.',
-    ]
+    ],
     CATEGORIES_2: [
         'Spesifikke fobier deles i fire undergrupper',
         'Fobi for dyr og insekter, for eksempel edderkopper, veps, slanger, katter og hunder.',
         'Fobi for naturfenomener, for eksempel tordenvær, vannskrekk og mørkeredsel.',
         'Fobi for skade og sykdom, for eksempel redsel for sprøytestikk, blod og tannlegeskrekk.',
         'Fobi for bestemte situasjoner, for eksempel klaustrofobi, høydeskrekk og flyskrekk.',
-    ]
+    ],
     CAUSES_1: 'Årsaken til fobier er oftest sammensatt. Det kan begynne med en panikklidelse der personen utvikler frykt for den situasjonen hvor hen fikk det første anfallet av panikk.',
     CAUSES_2: [
         'Tvillingsundersøkelser har vist at det foreligger en arvelig sårbarhet for å utvikle fobier. ',
         'Denne sårbarheten samvirker med ytre hendelser og kan gi fobi uten at det har forekommet et panikkanfall. ',
-    ]
+    ],
     SYMPTOMS_1: 'Symptomene er angst, uro, hjertebank, skjelving, ubehag i brystet, pustevanskeligheter, kvelningsfornemmelse, svimmelhet og følelse av uvirkelighet når personen utsettes for det fobiske stimulus.',
     SYMPTOMS_2: 'Frykt for å dø er et vanlig symptom.',
 }
